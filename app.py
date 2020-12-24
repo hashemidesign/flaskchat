b31ed75e7d73ebdd0730229623be24095fa4e2c3
Mohsen Hashemi <m.hashemi.code@gmail.com>
2020-12-24 07:32:40 +0330

    emit message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 
 app = Flask(__name__)
@@ -15,7 +15,14 @@ def home():
 
 @s_io.on('message')
 def received_message(message):
+    send('hi client. Im here...')
     print(message)
+
+
+@s_io.on('custom event')
+def receive_custom_event(message):
+    print(f'THE CUSTOM MESSAGE EMITTED FROM JS: {message}')
+    emit('from flask', 'this is from server emit')
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -2,4 +2,14 @@ let socket = io.connect('http://127.0.0.1:5500');
 
 socket.on('connect', function() {
     socket.send('page is now connected');
+
+    socket.emit('custom event', 'The custom event message');
+});
+
+socket.on('message', function (message) {
+    console.log(message);
+});
+
+socket.on('from flask', function (message) {
+    console.log(message);
 });
